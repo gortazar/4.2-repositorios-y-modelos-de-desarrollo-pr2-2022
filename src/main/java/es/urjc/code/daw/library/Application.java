d721f1a5ae2ab132208d6b7696e2861a9582127d
Patxi Gortázar <patxi.gortazar@gmail.com>
2022-06-07 17:55:33 +0200

    Add toggle LINE_BREAKER_FEATURE

--- src/main/java/es/urjc/code/daw/library/Application.java
+++ src/main/java/es/urjc/code/daw/library/Application.java
@@ -2,12 +2,20 @@ package es.urjc.code.daw.library;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.togglz.core.manager.EnumBasedFeatureProvider;
+import org.togglz.core.spi.FeatureProvider;
 
 @SpringBootApplication
 public class Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
+	}
+	
+	@Bean
+	public FeatureProvider featureProvider() {
+	    return new EnumBasedFeatureProvider(Features.class);
 	}
 
 }
---
+++ src/main/java/es/urjc/code/daw/library/Features.java
@@ -0,0 +1,11 @@
+package es.urjc.code.daw.library;
+
+import org.togglz.core.Feature;
+import org.togglz.core.annotation.Label;
+
+public enum Features implements Feature {
+	
+	@Label("Line breaker")
+	LINE_BREAKER_FEATURE
+
+}
