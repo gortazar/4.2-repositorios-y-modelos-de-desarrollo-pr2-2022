0d196e30884c7b228b8ffa44fe363e1cbcece4ad
Patxi Gortázar <patxi.gortazar@gmail.com>
2022-06-07 18:45:51 +0200

    Wire linebreaker, disabled by default

--- src/main/java/es/urjc/code/daw/library/book/BookService.java
+++ src/main/java/es/urjc/code/daw/library/book/BookService.java
@@ -4,7 +4,9 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.stereotype.Service;
+import org.togglz.core.manager.FeatureManager;
 
+import es.urjc.code.daw.library.Features;
 import es.urjc.code.daw.library.notification.NotificationService;
 
 /* Este servicio se usará para incluir la funcionalidad que sea 
@@ -13,12 +15,20 @@ import es.urjc.code.daw.library.notification.NotificationService;
 @Service
 public class BookService {
 
+	private static final int DEFAULT_LINE_LENGTH = 10;
 	private BookRepository repository;
 	private NotificationService notificationService;
+	private FeatureManager featureManager;
+	private LineBreaker lineBreaker;
 
-	public BookService(BookRepository repository, NotificationService notificationService){
+	public BookService(BookRepository repository, 
+			NotificationService notificationService,
+			LineBreaker lineBreaker,
+			FeatureManager featureManager){
 		this.repository = repository;
 		this.notificationService = notificationService;
+		this.lineBreaker = lineBreaker;
+		this.featureManager = featureManager;
 	}
 
 	public Optional<Book> findOne(long id) {
@@ -34,6 +44,9 @@ public class BookService {
 	}
 
 	public Book save(Book book) {
+		if(featureManager.isActive(Features.LINE_BREAKER_TOGGLE)) {
+			book.setDescription(lineBreaker.breakLine(book.getDescription(), DEFAULT_LINE_LENGTH));
+		}
 		Book newBook = repository.save(book);
 		notificationService.notify("Book Event: book with title="+newBook.getTitle()+" was created");
 		return newBook;
